ee56c0a76388cbbc974407eacf648adc0e92c566
sundargrng <gurungsundar5@gmail.com>
2024-02-03 16:37:27 +0545

    Enemy habitat (range)
    If the player is in the range of the enemy, enemy follows the player. If the player goes out of the range, enemy returns back to its habitat position i.e homePos.
    
    Also changed the grid size to 1 from 0.32, which made easier to calculate the distance between two objects and their transform position. And also all the sprite size from 100 pixels per unit to 32 pixels per unit.

--- Assets/EnemyController.cs
+++ Assets/EnemyController.cs
@@ -9,11 +9,16 @@ public class EnemyController : MonoBehaviour
 
     private Transform target; // the enemy follows the target if the target is in range. Target is player
 
+    public Transform homePos;
+
     [SerializeField] // Will allow us to change the speed anytime in the unity
     private float speed; // movement speed for enemy
 
     [SerializeField]
-    private float range;
+    private float maxRange;
+
+    [SerializeField]
+    private float minRange;
 
     // Start is called before the first frame update
     void Start()
@@ -27,17 +32,39 @@ public class EnemyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        followPlayer();
+        if (Vector3.Distance(target.position, transform.position) <= maxRange && Vector3.Distance(target.position, transform.position) >= minRange)
+        {
+            followPlayer();
+        }
+        else if(Vector3.Distance(target.position, transform.position)>= maxRange)
+        {
+            GoHome();
+        }
     }
+        
+
 
     public void followPlayer()
     {
         animator.SetBool("inRange", true);
 
         // when enemy follows the player, sets the animation to the direction of player
-        animator.SetFloat("moveX", (target.transform.position.x - transform.position.x));
-        animator.SetFloat("moveY", (target.transform.position.y - transform.position.y));
+        animator.SetFloat("moveX", (target.position.x - transform.position.x));
+        animator.SetFloat("moveY", (target.position.y - transform.position.y));
 
         transform.position = Vector3.MoveTowards(transform.position, target.transform.position, speed * Time.deltaTime);
+    }
+
+
+    public void GoHome()
+    {
+        animator.SetFloat("moveX", (homePos.position.x - transform.position.x));
+        animator.SetFloat("moveY", (homePos.position.y - transform.position.y));
+        transform.position = Vector3.MoveTowards(transform.position, homePos.position, speed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, homePos.position) == 0)
+        {
+            animator.SetBool("inRange", false);
+        }
     }
 }
