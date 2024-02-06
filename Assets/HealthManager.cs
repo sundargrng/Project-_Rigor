f069141dd52d102a663466fea203646ec1aa9f55
sundargrng <gurungsundar5@gmail.com>
2024-02-06 21:37:26 +0545

    Flash effect when player is hit by an enemy

--- Assets/HealthManager.cs
+++ Assets/HealthManager.cs
@@ -7,22 +7,61 @@ public class HealthManager : MonoBehaviour
 
     public int currentHealth;
     public int maxHealth;
+
+    private bool hurtFlash;
+
+    [SerializeField]
+    private float flashTime = 0f;
+    private float flashCountDown = 0f;
+
+    private SpriteRenderer playerSprite;
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerSprite = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (hurtFlash)
+        {
+            if (flashCountDown> flashTime * .99f)
+            {
+                playerSprite.color = new Color(playerSprite.color.r, playerSprite.color.g, playerSprite.color.b, 0f);
+            }
+            else if (flashCountDown > flashTime * .75f)
+            {
+                playerSprite.color = new Color(playerSprite.color.r, playerSprite.color.g, playerSprite.color.b, 1f);
+            }
+            else if (flashCountDown > flashTime * .50f)
+            {
+                playerSprite.color = new Color(playerSprite.color.r, playerSprite.color.g, playerSprite.color.b, 0f);
+            }
+            else if (flashCountDown > flashTime * .25f)
+            {
+                playerSprite.color = new Color(playerSprite.color.r, playerSprite.color.g, playerSprite.color.b, 1f);
+            }
+            else if (flashCountDown >  0f)
+            {
+                playerSprite.color = new Color(playerSprite.color.r, playerSprite.color.g, playerSprite.color.b, 0f);
+            }
+            else
+            {
+                playerSprite.color = new Color(playerSprite.color.r, playerSprite.color.g, playerSprite.color.b, 1f);
+                hurtFlash = false;
+            }
+            flashCountDown -= Time.deltaTime;
+        }
     }
 
 
     public void damagePlayer(int damageTaken)
     {
         currentHealth -= damageTaken;
+        hurtFlash = true;
+        flashCountDown = flashTime;
+
         if (currentHealth <= 0)
         {
             gameObject.SetActive(false);
