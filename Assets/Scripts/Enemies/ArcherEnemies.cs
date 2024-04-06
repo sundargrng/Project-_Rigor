f26e4162b390486894fd5d90f140e77578c912dd
sundargrng <gurungsundar5@gmail.com>
2024-04-06 19:19:50 +0545

    Further level design and setup and patrolling enemies
    Updated archer enemies to patrol around certain locations when not chasing the player

--- Assets/Scripts/Enemies/ArcherEnemies.cs
+++ Assets/Scripts/Enemies/ArcherEnemies.cs
@@ -18,62 +18,97 @@ public class ArcherEnemies : MonoBehaviour
 
     private Animator animator;
 
-    public Transform homePosition;
+    public Transform[] patrolPositions;
+    private int targetPoint;
 
-    // Start is called before the first frame update
+    private bool isWaitingAtPatrolPoint = false;
+
     void Start()
     {
-        player = FindAnyObjectByType<WarriorController>().transform;
+        player = GameObject.FindGameObjectWithTag("Player").transform;
         animator = GetComponent<Animator>();
+        targetPoint = 0;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        animator.SetBool("inAttackRange", true);
-
         float distanceFromPlayer = Vector2.Distance(player.position, transform.position);
 
-        if (distanceFromPlayer < lineOfSite && distanceFromPlayer>attackRange)
+        if (!isWaitingAtPatrolPoint)
         {
-            animator.SetBool("inRange", true);
-            animator.SetFloat("walkX", (player.position.x - this.transform.position.x));
-            animator.SetFloat("walkY", (player.position.y - this.transform.position.y));
-            transform.position = Vector2.MoveTowards(this.transform.position, player.position, speed * Time.deltaTime);
-        }
-
-        attackCoolDown += Time.deltaTime;
-
-        if (distanceFromPlayer < attackRange && attackCoolDown > attackAnimTime)
-        {
-            attackCoolDown = 0;
-
-            animator.SetBool("inAttackRange", true);
-            animator.SetFloat("lookX", (player.position.x - this.transform.position.x));
-            animator.SetFloat("lookY", (player.position.y - this.transform.position.y));
-
-            Instantiate(arrow, shootingRange.transform.position, Quaternion.identity);
-        }
-
-        if (distanceFromPlayer > attackRange && distanceFromPlayer<lineOfSite)
-        {
-            animator.SetBool("inAttackRange", false);
-            animator.SetFloat("walkX", (player.position.x - this.transform.position.x));
-            animator.SetFloat("walkY", (player.position.y - this.transform.position.y));
-            transform.position = Vector2.MoveTowards(this.transform.position, player.position, speed * Time.deltaTime);
-        }
-
-        if (distanceFromPlayer > lineOfSite)
-        {
-            animator.SetBool("inAttackRange", false);
-            animator.SetFloat("walkX", (homePosition.position.x - this.transform.position.x));
-            animator.SetFloat("walkY", (homePosition.position.y - this.transform.position.y));
-            transform.position = Vector2.MoveTowards(this.transform.position, homePosition.position, speed * Time.deltaTime);
-
-            if (Vector2.Distance(this.transform.position, homePosition.position)==0 )
+            if (distanceFromPlayer < lineOfSite && distanceFromPlayer > attackRange)
             {
-                animator.SetBool("inRange", false);
+                // Move towards the player
+                animator.SetBool("inRange", true);
+                animator.SetFloat("walkX", (player.position.x - transform.position.x));
+                animator.SetFloat("walkY", (player.position.y - transform.position.y));
+                transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
             }
+
+            attackCoolDown += Time.deltaTime;
+
+            if (distanceFromPlayer < attackRange && attackCoolDown > attackAnimTime)
+            {
+                // Attack if within attack range and cooldown is ready
+                attackCoolDown = 0;
+                animator.SetBool("inAttackRange", true);
+                animator.SetFloat("lookX", (player.position.x - transform.position.x));
+                animator.SetFloat("lookY", (player.position.y - transform.position.y));
+
+                Instantiate(arrow, shootingRange.transform.position, Quaternion.identity);
+            }
+
+            if (distanceFromPlayer > attackRange && distanceFromPlayer < lineOfSite)
+            {
+                // Move towards the player but not within attack range
+                animator.SetBool("inAttackRange", false);
+                animator.SetBool("inRange", true);
+                animator.SetFloat("walkX", (player.position.x - transform.position.x));
+                animator.SetFloat("walkY", (player.position.y - transform.position.y));
+                transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+            }
+
+            if (distanceFromPlayer > lineOfSite)
+            {
+                // Patrol towards patrol points when player is out of sight
+                animator.SetBool("inAttackRange", false);
+                animator.SetBool("inRange", true);
+
+                Vector2 targetPatrolPoint = patrolPositions[targetPoint].position;
+                animator.SetFloat("walkX", (targetPatrolPoint.x - transform.position.x));
+                animator.SetFloat("walkY", (targetPatrolPoint.y - transform.position.y));
+                transform.position = Vector2.MoveTowards(transform.position, targetPatrolPoint, speed * Time.deltaTime);
+
+                // Check if reached the current patrol point
+                if (Vector2.Distance(transform.position, targetPatrolPoint) < 0.1f)
+                {
+                    // Start waiting at patrol point
+                    StartCoroutine(WaitAtPatrolPoint());
+                }
+            }
+        }
+    }
+
+    IEnumerator WaitAtPatrolPoint()
+    {
+        isWaitingAtPatrolPoint = true;
+        animator.SetBool("lookAround", true);
+
+        yield return new WaitForSeconds(2.1f); // Wait for enemy to look around
+
+        // Move to the next patrol point
+        IncreaseTargetPointIndex();
+
+        animator.SetBool("lookAround", false);
+        isWaitingAtPatrolPoint = false; // Reset flag to allow movement
+    }
+
+    private void IncreaseTargetPointIndex()
+    {
+        targetPoint++;
+        if (targetPoint >= patrolPositions.Length)
+        {
+            targetPoint = 0; // Wrap around to the first patrol point if index exceeds array length
         }
     }
 
